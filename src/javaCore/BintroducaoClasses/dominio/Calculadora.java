265d94f58eb160ec2c47c7d300f8ba17658623fb
Edu <luis_100404@hotmail.com>
2025-02-26 09:19:38 -0300

    "add new files from introducing POO"

---
+++ src/javaCore/BintroducaoClasses/dominio/Calculadora.java
@@ -0,0 +1,13 @@
+package javaCore.BintroducaoClasses.dominio;
+
+public class Calculadora {
+    //métodosPt1
+    public void somaNumero(){
+        System.out.println(10 + 10);
+    }
+
+    public void subtraiNumero(){
+        System.out.println(20 - 10);
+    }
+
+}
---
+++ src/javaCore/BintroducaoClasses/test/CalculadoraTest01.java
@@ -0,0 +1,18 @@
+package javaCore.BintroducaoClasses.test;
+
+import javaCore.BintroducaoClasses.dominio.Calculadora;
+
+public class CalculadoraTest01 {
+
+    public static void main(String[] args) {
+        //métodosPt1
+        Calculadora calc = new Calculadora();
+
+        System.out.println("Soma:");
+        calc.somaNumero();
+
+        System.out.println("\nSubtração:");
+        calc.subtraiNumero();
+    }
+
+}
